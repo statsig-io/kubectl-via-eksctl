7f66a46af58c356d0c9522345b4e948fb4ba9aa8
roim <roim.rodrigo@gmail.com>
2021-03-12 20:08:37 -0800

    1.14 and more debugs

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine/k8s:1.18.2
+FROM alpine/k8s:1.14.9
 
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -l
 
-set -e
+set -xe
 
 export AWS_ACCESS_KEY_ID="$aws_access_key_id"
 export AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
