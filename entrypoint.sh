232c1062219a7ec00a02fad4033eb52a6dc9bbb4
roim <roim.rodrigo@gmail.com>
2021-03-12 20:12:28 -0800

    Fix version

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,8 @@ set -xe
 export AWS_ACCESS_KEY_ID="$aws_access_key_id"
 export AWS_SECRET_ACCESS_KEY="$aws_secret_access_key"
 
-eksctl --version
+echo "eksctl version"
+eksctl version
 
 echo "Writing kubeconfig"
 eksctl utils write-kubeconfig --region "$region" --cluster "$cluster"
